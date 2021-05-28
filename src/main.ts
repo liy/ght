1c4a107286e57fa11e7263e020ec0d7f829fc9b8
Zhengyi Li <liy8272@gmail.com>
2021-05-28 22:47:57 +0100

    Update parser to have options

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-const isOptionRegex = /^(?:--\w+|-\w)|--/;
-const endingWithDigits = /\d+$/;
+interface Option {
+  name: string;
+  value: string | number | boolean;
+}
 
-interface Command {
-  context: Array<string>;
-  options: Map<string, string | number | boolean>;
+export interface Command {
+  /**
+   * Arguments of the command
+   */
+  args: Array<string>;
+  /**
+   * Key value pairs of the the options
+   */
+  map: Map<string, string | number | boolean>;
+  /**
+   * Contains all the option pairs
+   */
+  options: Array<Option>;
+  /**
+   * Match and parse information of the command
+   */
+  matches: Array<Match>;
 }
 
 /**
@@ -11,7 +27,7 @@ interface Command {
  * @param value option text
  * @returns option value, boolean, string or number
  */
-function parseOption(option: string) {
+function parseOptionValue(option: string) {
   const num = Number(option);
   // boolean
   if (option === "" || option === undefined) {
@@ -28,6 +44,21 @@ function parseOption(option: string) {
   }
 }
 
+const isOptionRegex = /^(?:--\w+|-\w)|--/;
+// const hasEndDigitRegex = /-.(\d+)$/;
+const hasEndDigitRegex = /\d+$/;
+
+interface Pair {
+  key: string;
+  value: string | number | boolean | undefined;
+}
+
+export interface Match {
+  text: string;
+  index: number;
+  isOption: boolean;
+}
+
 /**
  * Split command input text using space as delimiter, ignore space inside quotes.
  * Option start with - or --
@@ -40,42 +71,73 @@ function parseOption(option: string) {
 function parse(text: string): Command {
   // Split by space which is not in quotes.
   // https://stackoverflow.com/questions/16261635/javascript-split-string-by-space-but-ignore-space-in-quotes-notice-not-to-spli
-  const matches = text.match(/(?:[^\s"]+|"[^"]*")+/g);
+  const results = [...text.matchAll(/(?:[^\s"]+|"[^"]*")+/g)];
 
-  const options = new Map<string, string | number | boolean>();
-  const context = new Array();
+  const map = new Map<string, string | number | boolean>();
+  const args = new Array();
+  const options = new Array<Option>();
 
-  let lastOptionKey: null | string = null;
+  const matches: Array<Match> = results.map((result, index) => {
+    const isOption = isOptionRegex.test(result[0]);
 
-  matches?.forEach((match) => {
-    const isOption = isOptionRegex.test(match);
+    return {
+      text: result[0],
+      index: result.index!,
+      isOption,
+    };
+  });
 
-    if (isOption) {
-      // options
-      const [key, value] = match.split("=");
-      const result = key.match(endingWithDigits);
-      if (result) {
-        options.set(key.substring(0, result.index), Number(result[0]));
-      } else {
-        options.set(key, parseOption(value));
+  matches.forEach((match, index) => {
+    if (match.isOption) {
+      const [name, value] = match.text.split("=");
+      // option contains equal sign followed by value.
+      if (value) {
+        const option = { name, value: parseOptionValue(value) };
+        map.set(name, value);
+        options.push({ name, value });
       }
-      lastOptionKey = key;
+      // option does not contains equal sign, but potentially ending with digits
+      else {
+        const digitResult = match.text.match(hasEndDigitRegex);
+        // end with digits
+        if (digitResult) {
+          const option = {
+            name: match.text.substring(0, digitResult.index),
+            value: Number(digitResult[0]),
+          };
+          map.set(option.name, option.value);
+          options.push({ name: option.name, value: option.value });
+        }
+        // not ending with digits, could potentially has space separate option value
+        else {
+          if (matches[index + 1] && !matches[index + 1].isOption) {
+            const value = parseOptionValue(matches[index + 1].text);
+            map.set(match.text, value);
+            options.push({ name: match.text, value });
+          } else {
+            map.set(match.text, true);
+            options.push({ name: match.text, value: true });
+          }
+        }
+      }
     } else {
-      // If last match is an option, then this will be
-      // the value of the last option.
-      if (lastOptionKey) {
-        options.set(lastOptionKey, parseOption(match));
-        lastOptionKey = null;
-      } else {
-        context.push(match);
+      // Do not treat the value as argument is it is used for option value
+      if (matches[index - 1] && !matches[index - 1].isOption) {
+        args.push(match.text);
       }
     }
   });
 
   return {
-    context,
+    args,
+    map,
+    matches,
     options,
   };
 }
 
 export default parse;
+
+export function isOption(text: string) {
+  return isOptionRegex.test(text);
+}
